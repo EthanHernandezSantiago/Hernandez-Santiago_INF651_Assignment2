3584d64fe25ad0a45b42f7e8cb64d2946c65adf8
EthanHernandezSantiago <112006623+EthanHernandezSantiago@users.noreply.github.com>
2022-09-18 21:43:00 -0500

    Added files

---
+++ 84272442/assigment2/main.js
@@ -0,0 +1,30 @@
+// Question 1
+let myNumber;
+
+//Question 2
+myNumber = Number(5);
+
+//Question 3
+let myOtherNumber = Number(5);
+
+//Question 4
+let myTotal = Number(myNumber * myOtherNumber);
+
+//Question 5
+let myName = String();
+let myCombo = String(myNumber + myOtherNumber);
+
+//Question 6
+let myOtherCombo = Number(myNumber + myOtherNumber);
+
+//Question 7
+let myRemaninder = Number(myNumber % 5);
+
+//Question 8
+let i = Number(2);
+
+//Question 9
+let int = Number(4);
+
+//Question 10
+let myBoolean = Boolean(false);
